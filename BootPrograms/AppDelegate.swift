8ef4ffbb71b3ce366f8c83fbfe7a88fad8984592
Fool <foolofshadows@gmail.com>
2019-10-07 15:22:40 -0500

    Initial Commit

---
+++ BootPrograms/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BootPrograms
+//
+//  Created by Fool on 10/7/19.
+//  Copyright © 2019 Fool. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
